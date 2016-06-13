e943c8d5181d97d26bd306bb62d864d20df46afc
root <root@erg34>
2016-06-12 21:38:31 -0400

    problem2.rb update

--- problem2.rb
+++ problem2.rb
@@ -1,14 +1,13 @@
-a = 1
-b = 2
+a = 0
+b = 1
 c = 0
 total=0
 
-while c<=4000000
-	c = a+b
-	a=b
-	b=c
-	if c %2 ==0
-	total +=c 
-	end
+while c <= 4000000
+	c = a + b
+	a = b
+	b = c
+	total +=c if c % 2 == 0
 end
+
 puts total
